3fc95c813e7df9e4866d6563cb67d5529b12881d
DevKosX <kosbarmohamed.31@gmail.com>
2025-10-14 17:28:25 +0200

    Test finalisé pour une seule, deux et plusieurs lettres

--- src/main/java/r5a08_findmyword/Score.java
+++ src/main/java/r5a08_findmyword/Score.java
@@ -6,23 +6,31 @@ public class Score {
     private int position;
     private Letter result = Letter.INCORRECT;
 
+
     public Score(String correct) {
         this.correct=correct;
     }
 
-    public Letter letter(int i) {
+    public Letter letter(int i){
         return result;
     }
 
-    public void assess(int position, String attempt) {
+    public void assess(int position, String attempt){
+        char attemptChar = attempt.charAt(position);
         if (isCorrectLetter(position, attempt)){
             result = Letter.CORRECT;
+        } else if (isPartiallyCorrect(attemptChar)) {
+            result = Letter.PART_CORRECT;
+        } else {
+            result = Letter.INCORRECT;
         }
     }
 
-    private boolean isCorrectLetter(int position, String attempt) {
+    private boolean isCorrectLetter(int position, String attempt){
         return this.correct.charAt(position) == attempt.charAt(position);
     }
 
-
+    private boolean isPartiallyCorrect(char c){
+        return correct.indexOf(c) != -1;
+    }
 }
